ccd0d0c2556ec6f940856ad734199fbd9be92ede
Brandon Lam <brandonywlam@gmail.com>
2020-04-23 14:06:17 -0700

    [lab] Product model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true, length: { minimum: 10 }
 end
