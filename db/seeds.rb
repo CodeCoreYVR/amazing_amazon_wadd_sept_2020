a26ccd04170e632a8c9701367b4c31af1cb1e083
Brandon Lam <brandonywlam@gmail.com>
2020-04-26 13:48:43 -0700

    [exercise] Reviews seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require_relative '../lib/stdout_helpers'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,30 +9,40 @@
 
 NUM_OF_USERS = 20
 NUM_OF_PRODUCTS = 1000
+NUM_OF_REVIEWS = 2
 
+Review.destroy_all()
 Product.destroy_all()
 User.destroy_all()
 
-NUM_OF_PRODUCTS.times do
-  created_at = Faker::Date.backward(365)
-  Product.create({
-    title: Faker::Cannabis.strain,
+NUM_OF_PRODUCTS.times do |x|
+  created_at = Faker::Date.backward(days: 365)
+  product = Product.create({
+    title: "#{Faker::Cannabis.strain}-#{rand(1_000_000_000)}",
     description: Faker::Cannabis.health_benefit,
     price: rand(100_000),
     created_at: created_at,
     updated_at: created_at
   })
+  NUM_OF_REVIEWS.times do
+    Review.create({
+      rating: rand(1..5),
+      body: Faker::Hacker.say_something_smart,
+      product: product
+    })
+  end
+  Stdout.progress_bar(NUM_OF_PRODUCTS, x, "█") { "Creating Products with Reviews" }
 end
 
-puts Cowsay.say("Created #{NUM_OF_PRODUCTS} products!", :sheep)
-
-NUM_OF_USERS.times do
-  User.create({
+NUM_OF_USERS.times do |x|
+  u = User.create({
     first_name: Faker::Games::SuperSmashBros.fighter,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email
   })
+  Stdout.progress_bar(NUM_OF_USERS, x, "█") { "Creating Users" }
 end
 
+puts Cowsay.say("Created #{NUM_OF_PRODUCTS} products with #{NUM_OF_REVIEWS} reviews each!", :sheep)
 puts Cowsay.say("Created #{NUM_OF_USERS}  users!", :turtle)
 
