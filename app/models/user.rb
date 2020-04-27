22e111baaec5c4b8b195a2930633ad2fe3f348c9
Brandon Lam <brandonywlam@gmail.com>
2020-04-27 14:52:53 -0700

    [lab] Amazon: User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   scope(:search, -> (query) { where("first_name ILIKE ? OR last_name ILIKE ? OR email ILIKE ?", "%#{query}%", "%#{query}%", "%#{query}%") })
   scope(:created_after, -> (date) { where("created_at < ?", "#{date}") })
 
+  has_many :products
+  has_many :reviews
+
+  def full_name
+    "#{first_name} #{last_name}".strip.capitalize!
+  end
 end
 
 # Generated this model using `rails g model user first_name:string last_name:string email:string`
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,25 +15,6 @@ Review.destroy_all()
 Product.destroy_all()
 User.destroy_all()
 
-NUM_OF_PRODUCTS.times do |x|
-  created_at = Faker::Date.backward(days: 365)
-  product = Product.create({
-    title: "#{Faker::Cannabis.strain}-#{rand(1_000_000_000)}",
-    description: Faker::Cannabis.health_benefit,
-    price: rand(100_000),
-    created_at: created_at,
-    updated_at: created_at
-  })
-  NUM_OF_REVIEWS.times do
-    Review.create({
-      rating: rand(1..5),
-      body: Faker::Hacker.say_something_smart,
-      product: product
-    })
-  end
-  Stdout.progress_bar(NUM_OF_PRODUCTS, x, "█") { "Creating Products with Reviews" }
-end
-
 NUM_OF_USERS.times do |x|
   u = User.create({
     first_name: Faker::Games::SuperSmashBros.fighter,
@@ -43,6 +24,32 @@ NUM_OF_USERS.times do |x|
   Stdout.progress_bar(NUM_OF_USERS, x, "█") { "Creating Users" }
 end
 
-puts Cowsay.say("Created #{NUM_OF_PRODUCTS} products with #{NUM_OF_REVIEWS} reviews each!", :sheep)
-puts Cowsay.say("Created #{NUM_OF_USERS}  users!", :turtle)
+users = User.all
+
+NUM_OF_PRODUCTS.times do |x|
+  created_at = Faker::Date.backward(days: 365)
+  product = Product.create({
+    title: "#{Faker::Cannabis.strain}-#{rand(1_000_000_000)}",
+    description: Faker::Cannabis.health_benefit,
+    price: rand(100_000),
+    user: users.sample,
+    created_at: created_at,
+    updated_at: created_at
+  })
+  NUM_OF_REVIEWS.times do
+    Review.create({
+      rating: rand(1..5),
+      body: Faker::Hacker.say_something_smart,
+      product: product,
+      user: users.sample
+    })
+  end
+  Stdout.progress_bar(NUM_OF_PRODUCTS, x, "█") { "Creating Products with Reviews" }
+end
+
+products = Product.all
+reviews = Review.all
+
+puts Cowsay.say("Created #{products.count} products with #{NUM_OF_REVIEWS} reviews each!", :sheep)
+puts Cowsay.say("Created #{users.count}  users!", :turtle)
 
