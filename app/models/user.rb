f2f461a79be83fadbeadf24e9254fa6555692bd1
Hano <hinsul3113@gmail.com>
2020-07-27 16:21:30 -0700

    [Lab] Test drive for User Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,16 @@ class User < ApplicationRecord
 
   has_many :news_articles, dependent: :nullify
 
+  # Validations
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
+
   scope(:created_after, -> (date) { where("created_at < ?", "#{date}") })
   scope(:search, -> (query) { where("first_name ILIKE ? OR last_name ILIKE ? OR email ILIKE ?", "%#{query}%", "%#{query}%", "%#{query}%") })
 
   def full_name
-    "#{first_name} #{last_name}".strip.capitalize!
+    "#{first_name} #{last_name}".strip.titleize
   end
 end
 
