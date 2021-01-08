61e004519cae7f7d8bd2e5f6f555ceeb9afa2692
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-01-07 20:35:07 -0800

    [lab] Fixed Bill Splitter calculations

--- app/controllers/bill_splitter_controller.rb
+++ app/controllers/bill_splitter_controller.rb
@@ -7,6 +7,6 @@ class BillSplitterController < ApplicationController
     tax = params[:tax]
     tip = params[:tip]
     num_of_people = params[:num_of_people]
-    @split_amount = amount.to_f * tax.to_f * tip.to_f / num_of_people.to_f
+    @split_amount = ( amount.to_f + (amount.to_f * (tax.to_f / 100)) + tip.to_f ) / num_of_people.to_f
   end
 end
